766361f899efb84a1b842068b394a45c53402ae2
Saad Umar <saad_umar93@yahoo.com>
2023-05-27 19:31:33 +0500

    simplify spy

--- ProperDependencyInjectionTests/RemoteFeedLoaderTests.swift
+++ ProperDependencyInjectionTests/RemoteFeedLoaderTests.swift
@@ -11,23 +11,31 @@ import ProperDependencyInjection
 
 final class RemoteFeedLoaderTests: XCTestCase {
     private class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
-            self.requestedURL = url
+            self.requestedURLs.append(url)
         }
     }
 
     func test_init_doesNotRequestDataFromURL() {
         let (_,client) = makeSUT()
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     func test_load_requestsDataFromURL() {
         let url = URL(string: "www.my-url.com")!
         let (sut,client) = makeSUT(url: url)
         sut.load()
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "www.my-url.com")!
+        let (sut,client) = makeSUT(url: url)
+        sut.load()
+        sut.load()
+        XCTAssertEqual(client.requestedURLs, [url,url])
     }
 
     //MARK: - Helpers
