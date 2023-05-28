0a147c6cf0ee1a7036e7923e40cadfd111161f9e
Saad Umar <saad_umar93@yahoo.com>
2023-05-28 17:26:01 +0500

    deliver error on client error

--- ProperDependencyInjectionTests/RemoteFeedLoaderTests.swift
+++ ProperDependencyInjectionTests/RemoteFeedLoaderTests.swift
@@ -12,8 +12,12 @@ import ProperDependencyInjection
 final class RemoteFeedLoaderTests: XCTestCase {
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        var error: Error?
         
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error)->Void) {
+            if let error = error {
+                completion(error)
+            }
             self.requestedURLs.append(url)
         }
     }
@@ -38,6 +42,17 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url,url])
     }
 
+    func test_load_deliversErrorOnClientError() {
+        let (sut,client) = makeSUT()
+        var capturedError: RemoteFeedLoader.Error?
+        client.error = NSError(domain: "Test", code: 0)
+        
+        sut.load { error in
+            capturedError = error
+        }
+        
+        XCTAssertEqual(capturedError,.connectivity)
+    }
     //MARK: - Helpers
     
     private func makeSUT(url:URL = URL(string: "www.my-url-2.com")!) -> (RemoteFeedLoader,HTTPClientSpy) {
